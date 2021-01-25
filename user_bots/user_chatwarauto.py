cccdeb501355acef7ba9593c4a63eeebe58aaca4
justadoll <stelzybol@protonmail.com>
2021-01-25 18:37:01 +0000

    new func 'krob_de_karavan' in user_chatautowars.py

--- user_bots/user_chatwarauto.py
+++ user_bots/user_chatwarauto.py
@@ -4,6 +4,7 @@ from emoji import emojize
 from config import api_id, api_hash
 from time import sleep
 import re
+#TODO: print time
 
 defence = emojize(":shield:Защита")
 hero = emojize(":sports_medal:Герой")
@@ -11,7 +12,7 @@ quests = emojize(":world_map:Квесты")
 
 def forest_gump(cli):
     if hero_stamina(cli) > 0:
-        print("[+]You have stamina, have we go!")
+        print("[+] You have stamina, have we go!")
         cli.send_message("ChatWarsBot", quests)
         msg1 = cli.get_messages("ChatWarsBot")
         sleep(2)
@@ -20,12 +21,12 @@ def forest_gump(cli):
             forest_gump(cli)
         else:
             msg_time = cli.get_messages("ChatWarsBot")
-            print("[+]Debug:", msg_time[0].message)
+            print("[+] Debug:", msg_time[0].message)
             msg_time = re.findall("\d", str(msg_time[0].text))
             msg_time = str(msg_time[0])
             msg_time = int(msg_time)
             msg_time *= 60
-            print("[+]NOTE: Time you gone forest (in seconds) ==", msg_time)
+            print("[+] Time you gone forest (in seconds) ==", msg_time)
             msg_time += 30
             sleep(msg_time)
             forest_gump(cli)
@@ -47,7 +48,33 @@ def hero_stamina(cli):
     stamina = int(stamina[0])
     return stamina
 
-with TelegramClient('#USERNAME#', api_id, api_hash) as client:
-    forest_gump(client)
+def krob_de_karavan(cli):
+    if hero_stamina(cli) > 1:
+        print("[+]You have stamina to robe karavan, have we go!")
+        cli.send_message("ChatWarsBot", quests)
+        sleep(1)
+        msg = cli.get_messages("ChatWarsBot")
+        if msg[0].click(1) == None:
+            print("[-]Click failed,trying again...")
+            krob_de_karavan(cli)
+        else:
+            msg_time = cli.get_messages("ChatWarsBot")
+            print(msg_time[0].message)
+            print("[+] OK, you near the karavan, wait 5 minutes")
+            tmp = (6*60)+5
+            sleep(tmp)
+            msg_time = cli.get_messages("ChatWarsBot")
+            print(msg_time[0].message)
+    else:
+        sleep(3)
+        print("[-]No stamina, go to sleep for 5 hours...")
+        cli.send_message("ChatWarsBot", defence)
+        sleep(18000)
+        krob_de_karavan(cli)
+
+
+with TelegramClient('gol0va4_l3na', api_id, api_hash) as client:
+    krob_de_karavan(client)
+    #forest_gump(client)
 
     client.run_until_disconnected()
