02fce41209b65ed50db6ff183d4a8f2cdb43f9f3
Parrot user <stelzybol@protonmail.com>
2021-02-04 17:22:01 +0000

    prints 2 loguru

--- user_bots/user_chatwarauto.py
+++ user_bots/user_chatwarauto.py
@@ -7,13 +7,13 @@ from time import sleep
 import re
 #from loguru import logger
 from os import popen
-#TODO: loguru
+from loguru import logger
 
 defence = emojize(":shield:Защита")
 hero = emojize(":sports_medal:Герой")
 quests = emojize(":world_map:Квесты")
 
-#logger.add("debug.log", format="{time} {level} {message}", level="DEBUG", rotation="5 MB", compression="zip")
+logger.add("debug.json", format="{time} {level} {message}", level="INFO", rotation="5 MB", compression="zip", serialize=True)
 
 def parse_sessions():
     """ Take to array all sessions from 'session/' dir """
@@ -25,27 +25,27 @@ def parse_sessions():
 def forest_gump(cli):
     """ Function that go client to forest while have stamina """
     if hero_stamina(cli) > 0:
-        print("[+] You have stamina, have we go!")
+        logger.info("Session have stamina, have we go!")
         cli.send_message("ChatWarsBot", quests)
         msg1 = cli.get_messages("ChatWarsBot")
         sleep(2)
         if msg1[0].click(0) == None:
-            print("[-]Click failed")
+            logger.error("Click failed")
             forest_gump(cli)
         else:
             msg_time = cli.get_messages("ChatWarsBot")
-            print("[+] Debug:", msg_time[0].message)
+            logger.debug(msg_time[0].message)
             msg_time = re.findall("\d", str(msg_time[0].text))
             msg_time = str(msg_time[0])
             msg_time = int(msg_time)
             msg_time *= 60
-            print("[+] Time you gone forest (in seconds) ==", msg_time)
+            logger.info("Time in forest :"+str(msg_time))
             msg_time += 30
             sleep(msg_time)
             return True
     else:
         sleep(3)
-        print("[-]No stamina, go to sleep for 5 hours...")
+        #logger.debug("No stamina, go to sleep for 5 hours...")
         cli.send_message("ChatWarsBot", defence)
         return False
 
@@ -63,25 +63,25 @@ def hero_stamina(cli):
 
 def krob_de_karavan(cli):
     """ Same as 'forest_gump' but client goes on karavan  """
+    #TODO: return True, False
     if hero_stamina(cli) > 1:
-        print("[+]You have stamina to robe karavan, have we go!")
+        logger.info("Session have stamina to robe karavan, have we go!")
         cli.send_message("ChatWarsBot", quests)
         sleep(1)
         msg = cli.get_messages("ChatWarsBot")
         if msg[0].click(1) == None:
-            print("[-]Click failed,trying again...")
+            logger.error("Click failed,trying again...")
             krob_de_karavan(cli)
         else:
             msg_time = cli.get_messages("ChatWarsBot")
-            print(msg_time[0].message)
-            print("[+] OK, you near the karavan, wait 5 minutes")
+            logger.info("Session near the karavan, wait 5 minutes")
             tmp = (6*60)+5
             sleep(tmp)
             msg_time = cli.get_messages("ChatWarsBot")
-            print(msg_time[0].message)
+            logger.info(msg_time[0].message)
     else:
         sleep(3)
-        print("[-]No stamina, go to sleep for 5 hours...")
+        logger.debug("No stamina, go to sleep for 5 hours...")
         cli.send_message("ChatWarsBot", defence)
         sleep(18000)
         krob_de_karavan(cli)
@@ -93,13 +93,13 @@ def main():
         sessdir = "sessions/"+session
         with TelegramClient(sessdir, api_id, api_hash) as client:
             full = client(GetFullUserRequest(session.split(".")[0]))
-            print(full.user.username,"in FOR!")
+            logger.info(full.user.username,"in FOR!")
             while forest_gump(client):
-                print(sessdir,"{continue...}")
+                logger.info(sessdir,"{continue...}")
                 continue
             else:
                 pass
-                print(sessdir,"{continue...}")
+                logger.debug(sessdir,"{pass...}")
 
 
         #client.run_until_disconnected()
