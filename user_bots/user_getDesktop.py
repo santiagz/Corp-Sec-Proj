000d74b1726dbeda03c99e42eb4177f3e10c493f
justadoll <stelzybol@protonmail.com>
2021-04-04 17:35:03 +0400

    added getDesktop file

---
+++ user_bots/user_getDesktop.py
@@ -0,0 +1,13 @@
+from telethon.sync import TelegramClient, events
+from config import api_id, api_hash
+from time import sleep
+from os import system
+from telethon.tl.functions.users import GetFullUserRequest
+with TelegramClient('sessions/some.session', api_id, api_hash) as client:
+    n = client(GetFullUserRequest('me'))
+    print(n.user.phone)
+    for i in range(5):
+        sleep(3)
+        system('clear')
+        for message in client.iter_messages('Telegram',limit=3):
+            print(message.text)
