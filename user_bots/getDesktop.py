ff3040b83a091768cdcaeff618102bec1e1bbb32
justadoll <stelzybol@protonmail.com>
2021-04-22 22:56:31 +0400

    phone2tg logging and insert to db

--- user_bots/getDesktop.py
+++ user_bots/getDesktop.py
@@ -3,11 +3,15 @@ from config import api_id, api_hash
 from time import sleep
 from os import system
 from telethon.tl.functions.users import GetFullUserRequest
-with TelegramClient('sessions/name.session', api_id, api_hash) as client:
-    n = client(GetFullUserRequest('me'))
-    print(n.user.phone)
-    for i in range(5):
-        sleep(3)
-        system('clear')
-        for message in client.iter_messages('Telegram',limit=3):
-            print(message.text)
+
+client = TelegramClient('sessions/name.session', api_id, api_hash)
+arr = ["Telegram"]
+@client.on(events.NewMessage(chats=(arr)))
+async def main(event):
+        n = await client(GetFullUserRequest('me'))
+        print("NUMBER:",n.user.phone)
+        new_msg = event.message
+        print(new_msg.message)
+
+client.start()
+client.run_until_disconnected()
--- user_bots/phone2tg.py
+++ user_bots/phone2tg.py
@@ -4,8 +4,21 @@ from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon.tl.functions.contacts import DeleteContactsRequest
 from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon import functions, types
-from config import api_id, api_hash
+from telethon.errors.rpcerrorlist import FloodWaitError
+from fake_config import api_id, api_hash
+from loguru import logger
+from re import findall
+import psycopg2
 import asyncio
+import datetime as dt
+import time
+
+logger.add("tg_parser.log", format="{time} {level} {message}", level="INFO", rotation="5 MB", compression="zip", serialize=True)
+
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="passwd",host="localhost")
+conn.autocommit = True #or commit after parsing?
+db_cur = conn.cursor()
+cleardb_payload = "delete from tg_parser *;"
 
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
@@ -36,25 +49,51 @@ def stupid_make_phone_list(mask):
         result.append(readystr)
     return result
 
+def parse_telegram(phone_list):
+    startime = dt.datetime.now()
+    logger.info(f"START TIME {startime}")
+
+    for i in phone_list:
+        time.sleep(5)
+        try:
+            result = client(functions.contacts.ImportContactsRequest(
+                contacts=[types.InputPhoneContact(
+                    client_id=1498215433,
+                    phone=f'+{i}',
+                    first_name='ragul',
+                    last_name='lugar'
+                )]
+            ))
+            if(result.imported == []):
+                #print(f"'{i}' didn`t registred or blocked")
+                logger.debug(f"{i} not registred of blocked")
+            else:
+                uid = result.imported[0].user_id
+                uusername = result.users[0].username
+                phn = result.users[0].phone
+                last_ent = client(functions.contacts.DeleteContactsRequest(id=[uid]))
+                fn = last_ent.users[0].first_name
+                ln = last_ent.users[0].last_name
+                payload = f"insert into tg_parser(uid,username,phone,fn,ln) VALUES({uid},\'{uusername}\',\'{phn}\',\'{fn}\',\'{ln}\');"
+                logger.debug(payload)
+                db_cur.execute(payload)
+        except FloodWaitError as FWE:
+            FWE = str(FWE)
+            delay = findall("\d+",FWE)
+            delay = int(delay[0])
+            logger.error(f"Flood Exception! Stoped at {i} number")
+            logger.error(f"Going to sleep for in {str(delay)} seconds")
+            endtime = dt.datetime.now()
+            logger.info(f"Script worked for {endtime - startime}")
+            time.sleep(delay)
+            #break
+
+    endtime = dt.datetime.now()
+    logger.info(f"END TIME {endtime - startime}")
+
 with TelegramClient("sessions/name.session",api_id, api_hash) as client:
-    client(ImportChatInviteRequest("QLodgkw2yVQXBUkn"))
-    hi = client.get_entity('https://t.me/joinchat/QLodgkw2yVQXBUkn')
+    #client(ImportChatInviteRequest("QLodgkw2yVQXBUkn"))
+    #hi = client.get_entity('https://t.me/joinchat/QLodgkw2yVQXBUkn')
 
-    phone_list = stupid_make_phone_list("38098078XXXX")
-
-    result = client(functions.contacts.ImportContactsRequest(
-        contacts=[types.InputPhoneContact(
-            client_id=1498215433,
-            phone='+PHONENUMBER',
-            first_name='1',
-            last_name='1'
-        )]
-    ))
-    uid = result.imported[0].user_id
-    uusername = result.users[0].username
-    phn = result.users[0].phone
-    print(uid,uusername,phn)
-    last_ent = client(functions.contacts.DeleteContactsRequest(id=[uid]))
-    fn = last_ent.users[0].first_name
-    ln = last_ent.users[0].last_name
-    print(fn,ln)
+    phone_list = stupid_make_phone_list("38098XXXXXXX")
+    parse_telegram(phone_list)
